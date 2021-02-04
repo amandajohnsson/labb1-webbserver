dc8be5eaf21d683eb02f0d3d0e42a6466f532a9d
amandajohnsson <amanda-johnsson@hotmail.com>
2021-02-04 11:08:04 +0100

    Fixat snygg webb

--- server.c
+++ server.c
@@ -7,10 +7,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/fcntl.h>
+#include <sys/sendfile.h>
 
 #define SERVER_PORT 12345
 #define BUF_SIZE 4096
 #define QUEUE_SIZE 10
+
+char web_page[] =
+    "HTTP/1.0 200 OK\r\n"
+    "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+    "<!DOCTYPE html>\r\n"
+    "<html><head><title>ShellUaveX</title>\r\n"
+    "<style>body {background-color: #FFA3FF }</style></head>\r\n"
+    "<body><center><h1>Kossan säger muuu!</h1><br>\r\n"
+    "<img src=\"test_bild.jpg\"></center></body></html>\r\n";
 
 void fatal(char *string)
 {
@@ -20,7 +30,7 @@ void fatal(char *string)
 
 int main(int argc, char *argv[])
 {
-    int s, b, l, fd, sa, bytes, on = 1;
+    int server, b, l, fd, sa, bytes, on = 1;
     char buf[BUF_SIZE];
     struct sockaddr_in channel;
 
@@ -33,43 +43,62 @@ int main(int argc, char *argv[])
 
     /* Passive open. wait for connection. */
 
-    s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); /*create socket*/
-    if (s < 0)
+    server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); /*create socket*/
+    if (server < 0)
         fatal("socket failed");
-    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on));
+    setsockopt(server, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on));
 
-    b = bind(s, (struct sockaddr *)&channel, sizeof(channel));
+    b = bind(server, (struct sockaddr *)&channel, sizeof(channel));
     if (b < 0)
+    {
         fatal("bind failed");
-
-    l = listen(s, QUEUE_SIZE); /*specify queue size */
+        close(server);
+    }
+    l = listen(server, QUEUE_SIZE); /*specify queue size */
     if (l < 0)
+    {
         fatal("listen failed");
+        close(server);
+    }
     /*Socket is now set up and bound.  Wait for connection and process it. */
     while (1)
     {
         printf("Waiting on connection\n");
-        sa = accept(s, 0, 0); /*block for connection request */
-        printf("Connected\n");
+        sa = accept(server, NULL, NULL); /*block for connection request */
         if (sa < 0)
             fatal("accept failed");
-
-        snprintf((char *)buf, sizeof(buf), "HTTP/1.0 200 OK\r\n\r\nHello");
-        write(sa, (char *)buf, strlen((char *)buf));
+        printf("Connected\n");
         read(sa, buf, BUF_SIZE); /*READ FILE NAME FROM SOCKET*/
         /* get and return the file */
+        //snprintf((char *)buf, sizeof(buf), "HTTP/1.0 200 OK\r\n\r\nHello");
+        //write(sa, (char *)buf, strlen((char *)buf));
 
-        fd = open(buf, O_RDONLY); /* open the file to be sent back */
-        if (fd < 0)
-            fatal("open failed");
-
-        while (1)
+        /*if (!strncmp(buf, "GET /favicon.ico", 16))
         {
-            bytes = read(fd, buf, BUF_SIZE); /*read from file*/
-            if (bytes <= 0)
-                break;             /*check for end of file */
-            write(sa, buf, bytes); /*write bytes to socket */
+            fd = open("favicon.ico", O_RDONLY); /* open the file to be sent back */
+        /*if (fd < 0)
+                fatal("open failed");
+            sendfile(sa, fd, NULL, BUF_SIZE);
+        }*/
+        if (!strncmp(buf, "GET /test_bild.jpg", 16))
+        {
+            fd = open("test_bild.jpg", O_RDONLY); /* open the file to be sent back */
+            if (fd < 0)
+                fatal("open failed");
+            sendfile(sa, fd, NULL, 10000);
         }
+        else
+        {
+            write(sa, web_page, sizeof(web_page) - 1);
+        }
+        /* while (1)
+        {
+            bytes = read(fd, buf, BUF_SIZE); /*read from file
+            if (bytes <= 0)
+                break;             /*check for end of file 
+            write(sa, buf, bytes); /*write bytes to socket 
+        }
+        */
         close(fd); /*close file*/
         close(sa); /*close connection*/
     }
